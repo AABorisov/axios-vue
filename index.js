b3924d6e24ab8f6f7db9fd5852b1766407d284a6
AABorisov <borisov.andrew.a@gmail.com>
2018-10-06 21:28:52 +0300

    v-if v-for v-on:click v-model

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        seen: true,
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ],
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('');
+        }
     }
 });
+
+app.todos.push({text: 'New item'});
